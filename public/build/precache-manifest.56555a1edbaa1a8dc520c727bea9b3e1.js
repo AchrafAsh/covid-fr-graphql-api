7cba1a2bb0f7850c936b3ea7c19e9b84a0ba3d19
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-04-16 12:25:30 +0200

    Add selector

---
+++ public/build/precache-manifest.56555a1edbaa1a8dc520c727bea9b3e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6292a50e6a0399371a4d9e4a8ad7da2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "412f026adbae115a1aef",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "39f66beb082542caeb0e",
+    "url": "/static/js/2.5edb3ef1.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/static/js/2.5edb3ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412f026adbae115a1aef",
+    "url": "/static/js/main.201ded97.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
