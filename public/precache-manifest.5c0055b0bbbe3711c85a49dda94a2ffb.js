65c0b0d3e22a9e447c29849546a51c5094b05832
AchrafAsh <achraf.ait-sidi-hammou@ensta-paris.fr>
2020-06-30 11:56:22 +0200

    Remove public from gitignore

---
+++ public/precache-manifest.5c0055b0bbbe3711c85a49dda94a2ffb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e0d836a6a8162ab0ec0f80e83111638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "092c93ca8e488650e75f",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "559f4f7f51ee60307b3f",
+    "url": "/static/css/main.b5a5d45e.chunk.css"
+  },
+  {
+    "revision": "092c93ca8e488650e75f",
+    "url": "/static/js/2.fd7d4870.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/static/js/2.fd7d4870.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "559f4f7f51ee60307b3f",
+    "url": "/static/js/main.f9fa2146.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
